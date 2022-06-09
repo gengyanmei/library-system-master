c0fa84352d215d73fc94615f7c16c4477ec2908b
gengyanmei <2287100291@qq.com>
2022-06-09 17:12:49 +0800

    commit message

---
+++ src/main/java/com/gym/po/TypeInfo.java
@@ -0,0 +1,38 @@
+package com.gym.po;
+
+import java.io.Serializable;
+
+public class TypeInfo implements Serializable {
+
+    private Integer id;
+    private String name;
+    private String remarks;
+    private static final long serialVersionUID = 1L;
+
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+
+}
